Add fixes and changes: - Data normalization - --silent argument - Default sweep metric to dev_b3_f1 - --eval_only_split to run script in inference-only mode - Flags for scheduler - Initial eval before training - Test set evaluation - Handle hyperparameter flags to script

pipeline/mlp_layer.py
@@ -39,13 +39,8 @@ class MLPLayer(torch.nn.Module):
                    ([nn.Dropout(p=dropout_p)] if not dropout_only_once else []) +
                    [nn.Linear(hidden_dim, hidden_dim)]) * (n_hidden_layers - 1) +
                   ([activation_fn(**activation_args)] + [nn.BatchNorm1d(hidden_dim)] if add_batchnorm else []) +
-                                                                             [nn.Dropout(p=dropout_p),
-                                                                             nn.Linear(hidden_dim, 1)])
+                  [nn.Dropout(p=dropout_p), nn.Linear(hidden_dim, 1)])
             )
 
     def forward(self, x):
-        y = self.mlp_model(x)
-        self.edge_weights = y
-        if self.training:
-            self.edge_weights.retain_grad()
-        return self.edge_weights
+        return self.mlp_model(x)

pipeline/model.py
@@ -24,7 +24,6 @@ class EntResModel(torch.nn.Module):
         self.uncompress_layer = UncompressTransformLayer()
         self.sdp_layer = SDPLayer(max_sdp_iters=50000)
         self.hac_cut_layer = HACCutLayer()
-        #self.trellis_cut_estimator = TrellisCutLayer()
 
     def forward(self, x, N, verbose=False):
         edge_weights = torch.squeeze(self.mlp_layer(x))
@@ -47,4 +46,5 @@ class EntResModel(torch.nn.Module):
             logger.info("Size of HAC Cut OP is %s", pred_clustering.size())
             logger.info("HAC Cut OP")
             logger.info(pred_clustering)
+
         return pred_clustering

pipeline/sdp_layer.py
@@ -12,14 +12,10 @@ logging.basicConfig(format='%(asctime)s - %(levelname)s - %(name)s -   %(message
 logger = logging.getLogger(__name__)
 
 class SDPLayer(torch.nn.Module):
-    # Taken from the ECC Clusterer class in ecc_layer.py
-    def __init__(self,
-                 max_sdp_iters: int, problem_N=None):
+    def __init__(self, max_sdp_iters: int):
         super().__init__()
         self.max_sdp_iters = max_sdp_iters
         self.num_ecc = 0
-        # TODO: Implement problem_N functionality which creates a standard large matrix once and just solves it
-        #       in the forward pass each time, rather than building it out each time
 
     def build_and_solve_sdp(self, W_val, N, verbose=False):
         # Initialize the cvxpy layer

pipeline/uncompress_layer.py
@@ -4,7 +4,6 @@ import math
 class UncompressTransformLayer(torch.nn.Module):
     def __init__(self):
         super().__init__()
-        self.uncompressed_matrix = None
 
     def forward(self, compressed_matrix, N, make_symmetric=False, ones_diagonal=False):
         device = compressed_matrix.get_device()
@@ -12,13 +11,13 @@ class UncompressTransformLayer(torch.nn.Module):
         if make_symmetric:
             sym_indices = torch.stack((torch.cat((triu_indices[0], triu_indices[1])),
                                        torch.cat((triu_indices[1], triu_indices[0]))))
-            self.uncompressed_matrix = (
+            uncompressed_matrix = (
                 torch.sparse_coo_tensor(sym_indices, torch.cat((compressed_matrix, compressed_matrix)),
                                         [N, N])).to_dense()
         else:
-            self.uncompressed_matrix = (torch.sparse_coo_tensor(triu_indices, compressed_matrix, [N, N])).to_dense()
+            uncompressed_matrix = (torch.sparse_coo_tensor(triu_indices, compressed_matrix, [N, N])).to_dense()
 
         if ones_diagonal:
-            self.uncompressed_matrix += torch.eye(N, device=device)
+            uncompressed_matrix += torch.eye(N, device=device)
 
-        return self.uncompressed_matrix
+        return uncompressed_matrix

s2and/data.py
@@ -16,6 +16,7 @@ from collections import defaultdict, Counter
 
 from sklearn.cluster import KMeans
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 
 from s2and.sampling import sampling, random_sampling
 from s2and.consts import (
@@ -124,10 +125,17 @@ class S2BlocksDataset(Dataset):
             n is the number of signatures in a S2 block and f is the number of pairwise features
     """
     def __init__(self, blockwise_data: Dict[str, Tuple[np.ndarray, np.ndarray, np.ndarray]],
-                 convert_nan=True, nan_value=-1):
+                 convert_nan=True, nan_value=-1, scale=False, scaler=None):
         self.blockwise_data = blockwise_data
         self.convert_nan = convert_nan
         self.nan_value = nan_value
+        self.scale = scale
+        self.scaler = scaler
+        if self.scale and self.scaler is None:
+            # Fit scaler on input data
+            all_X = np.concatenate(list(map(lambda x: x[0], self.blockwise_data.values())))
+            self.scaler = StandardScaler()
+            self.scaler.fit(all_X)
 
     def __len__(self):
         return len(self.blockwise_data.keys())
@@ -138,7 +146,8 @@ class S2BlocksDataset(Dataset):
         X, y, clusterIds = self.blockwise_data[dict_key]
         if self.convert_nan:
             np.nan_to_num(X, copy=False, nan=self.nan_value)
-
+        if self.scale and self.scaler is not None:
+            X = self.scaler.transform(X)
         return X, y, clusterIds
 
 class ANDData: