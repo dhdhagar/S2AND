363d9934996da4b27965c598ba8e25beb6b2d3a9
Pragya Prakash <pragyaprakas@umass.edu>
2023-01-18 22:34:40 +0530

    training s2and model

--- e2e_scripts/train_s2and_hac.py
+++ e2e_scripts/train_s2and_hac.py
@@ -15,44 +15,49 @@ logging.basicConfig(format='%(asctime)s - %(levelname)s - %(name)s -   %(message
                     level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-dataset_name = "arnetminer"
-dataset_seed = 1
-parent_dir = f"../data/{dataset_name}"
-train_pkl = f"{PREPROCESSED_DATA_DIR}/{dataset_name}/seed{dataset_seed}/train_features.pkl"
-val_pkl = f"{PREPROCESSED_DATA_DIR}/{dataset_name}/seed{dataset_seed}/val_features.pkl"
-test_pkl = f"{PREPROCESSED_DATA_DIR}/{dataset_name}/seed{dataset_seed}/test_features.pkl"
+def load_training_data(train_pkl, val_pkl):
+    blockwise_data: Dict[str, Tuple[np.ndarray, np.ndarray]]
+    with open(train_pkl, "rb") as _pkl_file:
+        blockwise_data = pickle.load(_pkl_file)
 
-def load_training_data(pkl):
-    # blockwise_data: Dict[str, Tuple[np.ndarray, np.ndarray]]
-    # with open(pkl, "rb") as _pkl_file:
-    #     blockwise_data = pickle.load(_pkl_file)
-    #
-    # # Combine the blockwise_data to form train, test, val sets
-    # X_train = []
-    # y_train = []
-    #
-    # for block_data in blockwise_data.values:
-    #     x, y, cluster_ids =
+    # Combine the blockwise_data to form complete train, test, val sets
+    X_train = []
+    y_train = []
 
+    for block_data in blockwise_data.values:
+        x, y, cluster_ids = block_data
+        X_train = np.concatenate((X_train, x), axis=0)
+        y_train = np.concatenate((y_train, y), axis=0)
 
-    dataset = ANDData(
-        signatures=join(parent_dir, f"{dataset_name}_signatures.json"),
-        papers=join(parent_dir, f"{dataset_name}_papers.json"),
-        mode="train",
-        specter_embeddings=join(parent_dir, f"{dataset_name}_specter.pickle"),
-        clusters=join(parent_dir, f"{dataset_name}_clusters.json"),
-        block_type="s2",
-        name=dataset_name,
-        n_jobs=4,
-    )
-    logger.info("loaded the data")
+    blockwise_data_val: Dict[str, Tuple[np.ndarray, np.ndarray]]
+    with open(val_pkl, "rb") as _pkl_file:
+        blockwise_data_val = pickle.load(_pkl_file)
+
+    # Combine the blockwise_data to form complete train, test, val sets
+    X_val = []
+    y_val = []
+
+    for block_data in blockwise_data_val.values:
+        x, y, cluster_ids = block_data
+        X_val = np.concatenate((X_val, x), axis=0)
+        y_val = np.concatenate((y_val, y), axis=0)
+
+    logger.info("Dataset loaded and prepared for training")
+
+    # dataset = ANDData(
+    #     signatures=join(parent_dir, f"{dataset_name}_signatures.json"),
+    #     papers=join(parent_dir, f"{dataset_name}_papers.json"),
+    #     mode="train",
+    #     specter_embeddings=join(parent_dir, f"{dataset_name}_specter.pickle"),
+    #     clusters=join(parent_dir, f"{dataset_name}_clusters.json"),
+    #     block_type="s2",
+    #     name=dataset_name,
+    #     n_jobs=4,
+    # )
+    # logger.info("loaded the data")
 
     # Training Featurizer model
     featurization_info = FeaturizationInfo()
-    # the cache will make it faster to train multiple times - it stores the features on disk for you
-    train, val, _ = featurize(dataset, featurization_info, n_jobs=4, use_cache=True)
-    X_train, y_train, _ = train
-    X_val, y_val, _ = val
 
     logger.info("Done loading and featurizing")
     return featurization_info, X_train, y_train, X_val, y_val
@@ -70,8 +75,9 @@ def train_pairwise_classifier(featurization_info, X_train, y_train, X_val, y_val
     # this will also dump a lot of useful plots (ROC, PR, SHAP) to the figs_path
     pairwise_metrics = pairwise_eval(X_val, y_val, pairwise_model.classifier, figs_path='figs/', title='validation_metrics')
     logger.info(pairwise_metrics)
+    return pairwise_model
 
-def train_HAC_clusterer(featurization_info, pairwise_model):
+def train_HAC_clusterer(dataset_name, featurization_info, pairwise_model):
     clusterer = Clusterer(
         featurization_info,
         pairwise_model,
@@ -87,3 +93,14 @@ def train_HAC_clusterer(featurization_info, pairwise_model):
     logger.info(metrics)
 
 
+if __name__=='__main__':
+    dataset_name = "pubmed"
+    dataset_seed = 1
+    parent_dir = f"../data/{dataset_name}"
+    train_pkl = f"{PREPROCESSED_DATA_DIR}/{dataset_name}/seed{dataset_seed}/train_features.pkl"
+    val_pkl = f"{PREPROCESSED_DATA_DIR}/{dataset_name}/seed{dataset_seed}/val_features.pkl"
+    test_pkl = f"{PREPROCESSED_DATA_DIR}/{dataset_name}/seed{dataset_seed}/test_features.pkl"
+
+    featurization_info, X_train, y_train, X_val, y_val = load_training_data(train_pkl, val_pkl)
+    pairwise_model = train_pairwise_classifier(featurization_info, X_train, y_train, X_val, y_val)
+    train_HAC_clusterer(dataset_name, featurization_info, pairwise_model)
