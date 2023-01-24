1506ec9f552bd662cb92ffad9fcff30e5acfe7dd
Dhruv Agarwal <dhruv.agarwal17@gmail.com>
2023-01-24 11:30:51 +0530

    Modify experiment scripts - Add sweep files for all models - Modify sweep script to take in model type

--- run_sweep.sh
+++ run_sweep.sh
@@ -3,16 +3,17 @@
 dataset=${1:-"pubmed"}
 n_seeds=${2:-5}
 gpu_name=${3:-"gypsum-1080ti"}
+model=${4:-"e2e"}  # Used as prefix and to pick up the right sweep file
 
 for ((i = 1; i <= ${n_seeds}; i++)); do
-  JOB_DESC=${dataset}_sweep${i} && JOB_NAME=${JOB_DESC}_$(date +%s) && \
+  JOB_DESC=${model}_${dataset}_sweep${i} && JOB_NAME=${JOB_DESC}_$(date +%s) && \
   sbatch -J ${JOB_NAME} -e jobs/${JOB_NAME}.err -o jobs/${JOB_NAME}.log \
     --partition=${gpu_name} --gres=gpu:1 --mem=80G --time=12:00:00 \
     run_sbatch.sh e2e_scripts/train.py \
     --dataset="${dataset}" \
     --dataset_random_seed=${i} \
-    --wandb_sweep_name="main_${dataset}_${i}" \
-    --wandb_sweep_params="wandb_configs/sweeps/e2e_main.json" \
+    --wandb_sweep_name="${model}_${dataset}_${i}" \
+    --wandb_sweep_params="wandb_configs/sweeps/${model}.json" \
     --skip_initial_eval --silent
   echo "    Logs: jobs/${JOB_NAME}.err"
 done
