404a6fe24de1e5b0d0e9f1e8c3035f634c209440
Dhruv Agarwal <dhruv.agarwal17@gmail.com>
2023-03-10 10:19:47 -0500

    Add sweep prefix option to run_sweep

--- run_sweep.sh
+++ run_sweep.sh
@@ -4,6 +4,7 @@ dataset=${1:-"pubmed"}
 n_seeds=${2:-5}
 model=${3:-"e2e"}  # Used as prefix and to pick up the right sweep file
 gpu_name=${4:-"gypsum-1080ti"}
+sweep_prefix=${5:-""}
 
 for ((i = 1; i <= ${n_seeds}; i++)); do
   JOB_DESC=${model}_${dataset}_sweep${i} && JOB_NAME=${JOB_DESC}_$(date +%s) && \
@@ -15,7 +16,7 @@ for ((i = 1; i <= ${n_seeds}; i++)); do
     --pairwise_eval_clustering="both" \
     --skip_initial_eval \
     --silent \
-    --wandb_sweep_name="${model}_${dataset}_${i}" \
+    --wandb_sweep_name="${sweep_prefix}${model}_${dataset}_${i}" \
     --wandb_sweep_params="wandb_configs/sweeps/${model}.json" \
     --wandb_tags="${model},${dataset},seed_${i}"
   echo "    Logs: jobs/${JOB_NAME}.err"
