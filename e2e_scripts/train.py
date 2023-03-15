8154e1ce7008e277b6e6bba46bdd7eec63df1f6b
Dhruv Agarwal <dhruv.agarwal17@gmail.com>
2023-03-15 19:20:33 -0400

    Handle case when trained model is never better than untrained

--- e2e_scripts/train.py
+++ e2e_scripts/train.py
@@ -363,7 +363,7 @@ def train(hyperparams={}, verbose=False, project=None, entity=None, tags=None, g
                     scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=hyp['lr_step_size'],
                                                                 gamma=hyp['lr_gamma'], verbose=True)
 
-            best_dev_state_dict = None
+            best_dev_state_dict = copy.deepcopy(model.state_dict())
             best_dev_score = -1  # Stores the score of only the specified optimization metric
             best_dev_scores = None  # Contains scores of all metrics
             best_epoch = 0
