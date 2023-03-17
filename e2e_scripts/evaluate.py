bbca8504ce64d5693397d612f9c44f074fd4c154
Dhruv Agarwal <dhruv.agarwal17@gmail.com>
2023-03-17 01:57:52 -0400

    Add `eval_all` to run inference on test set using all methods

--- e2e_scripts/evaluate.py
+++ e2e_scripts/evaluate.py
@@ -134,7 +134,7 @@ def evaluate_pairwise(model, dataloader, overfit_batch_idx=-1, mode="macro", ret
                 # Forward pass through the e2e model
                 data = data.to(device)
                 try:
-                    edge_weights = model(data, warmstart=True, verbose=verbose)
+                    edge_weights = model(data, N=block_size, warmstart=True, verbose=verbose)
                     pred_cluster_ids = clustering_fn(edge_weights, block_size, min_id=(max_pred_id + 1),
                                                      threshold=clustering_threshold)
                 except CvxpyException as e:
