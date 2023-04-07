99ea970665b165bbd55832c921da727e0b427fc2
Dhruv Agarwal <dhruv.agarwal17@gmail.com>
2023-04-07 19:11:31 -0400

    Experiments script changes

--- experiments/feats_8.sh
+++ experiments/feats_8.sh
@@ -1,13 +1,14 @@
 #!/bin/bash -e
 
 sweep_prefix="feats8"
+log_fpath="./experiments/logs/${sweep_prefix}_$(date +%s).txt"
 
 dataset="pubmed"
 declare -a models=("e2e" "e2e-nosdp" "frac" "frac-nosdp" "mlp")
 gpu_name="gypsum-m40"
 for model in "${models[@]}"
 do
-   ./experiments/_feats_8.sh ${dataset} 1 5 ${model} ${gpu_name} ${sweep_prefix}
+   ./experiments/_feats_8.sh ${dataset} 1 5 ${model} ${gpu_name} ${sweep_prefix} >> ${log_fpath}
 done
 
 dataset="qian"
@@ -15,7 +16,7 @@ declare -a models=("e2e" "e2e-nosdp" "frac" "frac-nosdp" "mlp")
 gpu_name="gypsum-2080ti"
 for model in "${models[@]}"
 do
-   ./experiments/_feats_8.sh ${dataset} 1 5 ${model} ${gpu_name} ${sweep_prefix}
+   ./experiments/_feats_8.sh ${dataset} 1 5 ${model} ${gpu_name} ${sweep_prefix} >> ${log_fpath}
 done
 
 dataset="arnetminer"
@@ -23,5 +24,5 @@ declare -a models=("e2e" "e2e-nosdp" "frac" "frac-nosdp" "mlp")
 gpu_name="gypsum-1080ti"
 for model in "${models[@]}"
 do
-   ./experiments/_feats_8.sh ${dataset} 1 5 ${model} ${gpu_name} ${sweep_prefix}
+   ./experiments/_feats_8.sh ${dataset} 1 5 ${model} ${gpu_name} ${sweep_prefix} >> ${log_fpath}
 done
